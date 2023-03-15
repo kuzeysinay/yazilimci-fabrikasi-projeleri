bed3d08b46db5c8bc992610a92d05ebd12511a0e
Kuzey Sınay <kuzeysinay@gmail.com>
2023-03-16 00:33:46 +0300

    fix

--- redux-intro/src/redux/actions/counterActions.js
+++ redux-intro/src/redux/actions/counterActions.js
@@ -1,16 +1,16 @@
 import * as actionTypes from './actionTypes';
 
-export const increaseCount=()=>{
-    type: actionTypes.INCREASE
+export const increaseCount=()=>({
+    type: actionTypes.INCREASE,
     payload: 1
-}
+})
 
-export const decreaseCount = ()=>{
-    type: actionTypes.DECREASE
+export const decreaseCount = ()=>({
+    type: actionTypes.DECREASE,
     payload: 1
-}
+})
 
-export const increaseBy2Count = ()=>{
-    type: actionTypes.INCREASE_BY_2
+export const increaseBy2Count = ()=>({
+    type: actionTypes.INCREASE_BY_2,
     payload: 2
-}
+})
--- redux-intro/src/redux/reducers/index.js
+++ redux-intro/src/redux/reducers/index.js
@@ -3,7 +3,7 @@ import counterReducer from "./counterReducer";
 
 const reducers = combineReducers(
     {
-        counterReducer:counterReducer
+        counterReducer
     }
 );
 
