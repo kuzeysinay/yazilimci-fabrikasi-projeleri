b469fd64cd2c1274cc31ddb31d9682b22f311bc8
Kuzey Sınay <kuzeysinay@gmail.com>
2023-03-17 16:36:57 +0300

    accessed state

--- fake_store/src/App.js
+++ fake_store/src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './containers/Header';
+import ProductListing from './containers/ProductListing';
+import ProductDetail from './containers/ProductDetail';
+
+
 
 function App() {
   return (
     <div className="App">
-      <h1>hello</h1>
-      
+      <Router>
+        <Header/>
+        <Routes>
+          <Route path="/" exact element={<ProductListing/>}/>
+          <Route path="/product/:productId" exact element={<ProductDetail/>}/>
+          <Route>404 Not Found!</Route>
+        </Routes>
+      </Router>      
     </div>
   );
 }
