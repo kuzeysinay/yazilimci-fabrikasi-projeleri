d873c8380abfeee2ef744189e6490232ae6318d6
Kuzey Sınay <kuzeysinay@gmail.com>
2023-03-17 18:33:01 +0300

    rendered components

--- fake_store/src/redux/reducers/productReducer.js
+++ fake_store/src/redux/reducers/productReducer.js
@@ -2,18 +2,14 @@ import { ActionTypes } from "../constants/action-types";
 
 const initialState = {
   products: [
-    {
-      id: 1,
-      title: 'Kuzey',
-      category: 'Programmer'
-    },
+    
   ],
 };
 
 export const productReducer = (state = initialState, { type, payload }) => {
   switch (type) {
     case ActionTypes.SET_PRODUCTS:
-      return state;
+      return {...state, products: payload};
 
     default:
       return state;
