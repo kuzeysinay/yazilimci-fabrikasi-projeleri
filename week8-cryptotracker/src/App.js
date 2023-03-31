637e161b6cac9d5f3b872449367666bed8bf6c92
Kuzey Sınay <kuzeysinay@gmail.com>
2023-04-01 01:34:32 +0300

    second commit

--- week8-cryptotracker/src/App.js
+++ week8-cryptotracker/src/App.js
@@ -13,6 +13,62 @@ export default function App(){
     } catch(e){
       alert("Api error");
     }
-  }
+  };
+  useEffect(()=>{
+    fetchCoins();
+  }, []);
+
+  const columns = [
+    {
+      title: "Name",
+      key: "name",
+      width: 200,
+      render: (value, object) =>{
+        return(
+          <div style={{ display: "flex", alignItems: "center" }}>
+            <img height="20px" width="20px" src={object?.image} alt="coin" />
+            <span style={{ marginLeft: "10px" }}>{object?.name}</span>
+          </div>
+        );
+      },
+    },
+    {
+      title: "Symbol",
+      center: true,
+      key: "symbol",
+      width: 120,
+      render: (value, object) => <div>{value.toUpperCase()}</div>,
+    },
+    {
+      title: "Price",
+      key: "current_price",
+      width: 100,
+      render: (value, object) => <div>{`$${value}`}</div>,
+    },
+    {
+      title: "Change",
+      key: "price_change_percentage_24h",
+      width: 100,
+      render: (value, object) => {
+        return (
+          <div style={{ color: /-/i.test(value) ? "#ff0374" : "#06a847" }}>
+            {value}%
+          </div>
+        );
+      },
+    },
+  ]
+
+  return (
+    <EzReactTable
+      cols={columns}
+      data={coins}
+      darkMode
+      title="Crypto Tracker"
+      defaultSort="name"
+      accentColor="#ffbc03"
+      tableHeight={300}
+    />
+  );
 
 }
